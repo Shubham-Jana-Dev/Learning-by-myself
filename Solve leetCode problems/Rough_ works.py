1810735921b3033417b58a94e88b3ac8f1143bde
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2025-12-29 20:52:49 +0530

    refactor: implement value-based search with early exit
    Update condition to break when target value h is encountered
    Optimize loop performance by avoiding unnecessary iterations
    Maintain robust index boundary checks to prevent errors

--- Solve leetCode problems/Rough_ works.py
+++ Solve leetCode problems/Rough_ works.py
@@ -10,6 +10,8 @@ while i < len(f):
     print(f[i])
     i = i + 1
 '''
+# refactor: switch from index-based to value-based break
+"""
 f = (1, 3, 2, 4, 5, 6, 7, 8, 9, 10)
 t = 4
 idx = 0
@@ -18,3 +20,12 @@ while idx < len(f):
         break
     print(f[idx])
     idx += 1
+"""
+f = (10, 20, 4, 30)
+h = 4
+i = 0
+while i < len(f):
+    if(f[i] == 4):
+        break
+    print(f[i])
+    i += 1
