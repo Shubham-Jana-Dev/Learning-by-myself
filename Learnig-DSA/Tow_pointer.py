db48199e2a555b76da2b26110ccb1a221b652d30
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-12 09:41:35 +0530

    feat: understand the 3 sum problem and solve it.

--- Learnig-DSA/Tow_pointer.py
+++ Learnig-DSA/Tow_pointer.py
@@ -15,3 +15,25 @@ while(left<right):
     else:
         right -= 1
 '''
+# Logic to find a Triple Sum
+arr = [1,2,3,4,5,6,7,8,9]
+left = 0
+right = len(arr) -1
+target = 10
+arr.sort()
+for i in range(len(arr)-2):
+    if i>0 and arr[i] == arr[i -1]:
+        continue
+    current_target = target - arr[i]
+    left = i + 1
+    right = len(arr) - 1
+    while(left<right):
+        current_sum = arr[left] + arr[right]
+        if (current_sum == current_target):
+            print(f"Found: {arr[i]}, {arr[left]}, {arr[right]}")
+            left += 1
+            right -= 1
+        elif(current_sum<current_target):
+            left += 1
+        else:
+            right -= 1
