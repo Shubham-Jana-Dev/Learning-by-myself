00a5339a8f3b6def6f0671398fc88635f02b8eb2
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-30 19:36:09 +0530

    solve a new challenge which can fillter unwanted words from a list of string

--- Coding_challenge_from_friends/evening_chllenge.py
+++ Coding_challenge_from_friends/evening_chllenge.py
@@ -13,4 +13,28 @@ def input_Validator():
     else:
         print("Neutral State please provide a standard command like (\"Active\" or \"valid\" or \"Error\" or \"Blocked\").")
         input_Validator()
-input_Validator()
+#input_Validator()
+"""
+Challenge #51: The "Firewall" Filter
+​The Goal: Write a program that takes a list of various strings and creates two new lists: one for Authorized Inputs and one for Blocked Threats.
+​Technical Requirements:
+​Create a list called incoming_data containing at least 6 strings (e.g., "apple", "logic", "dirty", "coding", "doll", "university").
+​Create two empty lists: authorized_list and blocked_list.
+​Use a for loop to iterate through incoming_data.
+​Logic: * If the word is "dirty" or "doll", append it to the blocked_list.
+​Otherwise, append it to the authorized_list.
+​Print the final counts of each list.
+​Why this matters right now:
+"""
+def Firewall_filter():
+    input_list = ["apple","logic","dirty","coding","doll","university"]
+    blocked_list = []
+    authorized_list = []
+    for i in input_list:
+        if i == "dirty" or i == "doll":
+            blocked_list.append(i)
+        else:
+            authorized_list.append(i)
+    print(blocked_list)
+    print(authorized_list)
+Firewall_filter()
