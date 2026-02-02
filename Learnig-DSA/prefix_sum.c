dda3b086558f61559a7bf8d2585634ee37bc9a65
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-02 17:17:28 +0530

    feat: calculating prefix sum array using the function

--- Learnig-DSA/prefix_sum.c
+++ Learnig-DSA/prefix_sum.c
@@ -1,32 +1,28 @@
 #include<stdio.h>
-void calculateSqure (int number){
-    int squre = number*number;
-    printf("\nthe squre of %d is %d\n",number,squre);
-}
+void prefixSumArray(int arri[],int n,int count,int i){
+        while (i < n)
+    {
+        count += arri[i];
+        arri[i] = count;
+        i++;
+    }
+    printf("\nlength: %d\n",n);
+    printf("[");
+    for(int j=0; j<n;j++){
+        if(j<n-1){
+            printf(" %d,",arri[j]);
+        }
+        else{
+            printf(" %d",arri[j]);
+        }
+    }
+    printf("]");
+    }
 int main(){
-int arr[] = {1,2,3,4,5};
-int n = sizeof(arr)/sizeof(arr[0]);
-int count = 0;
-int i = 0 ;
-int arr1[n];
-while (i < n)
-{
-    count += arr[i];
-    printf("\n%d",count);
-    arr1[i] = count;
-    i++;
-}
-printf("\nlength: %d\n",n);
-printf("[");
-for(int j=0; j<n;j++){
-    if(j<n-1){
-        printf(" %d,",arr1[j]);
-    }
-    else{
-        printf(" %d",arr1[j]);
-    }
-}
-printf("]");
-calculateSqure(6);
+    int arr1[] = {1,2,3,4,5};
+    int n = sizeof(arr1)/sizeof(arr1[0]);
+    int count = 0;
+    int i = 0 ;
+    prefixSumArray(arr1,n,count,i);
     return 0;
 }
