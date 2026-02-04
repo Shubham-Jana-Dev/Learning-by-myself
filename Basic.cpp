51194fa507e48953464f9e4d9d6f00e51231c974
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-04 06:12:49 +0530

    feat: understand the declaration of array in c++ and print the array by the latest C++ 11 update (using int x:array)

--- Basic.cpp
+++ Basic.cpp
@@ -76,27 +76,75 @@ int ForLoop(int num){    // function 2
     }
     return fact;
 }
+int findMax(int num1,int num2,int num3){
+    if (num1>num2 && num1 > num3){
+        return num1;
+    }
+    else if (num2>num3){
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+void dynamicPartten(int row_num){
+    for(int i = 0; i <= row_num; i++){
+        for (int j = 0; j<=row_num;j++){
+            cout << "* ";
+        }
+    cout << " "<<endl;
+    }
+}
+void dynamicRightangleTriangle(int rows_num){
+    for(int i = 0; i <= rows_num; i++){
+        for(int j = 0; j <= rows_num; j++){
+            if(i+j >= rows_num){
+                cout << "* ";
+            }
+        }
+    cout << " "<<endl;
+    }
+}
+void arrayPrint(int arr[],int length){
+    for(int i = 0; i < length; i++){
+        cout << arr[i] <<" ";
+    }
+}
 int main(){
-// int result = fact(5);  // function 1
-// cout << result;
-// int num;
-// cout << "Enter the number: ";
-// cin >> num;
-// int result = ForLoop(num);    // function 2
-// cout << result<< endl;
-float num1 = 45.73; // by default c++ treats this as a double
-float num2 = 45.73f; // we use f for tell the compiler that it's a floating point value
-long double num3 = 45.73l; // we use l for tell the compiler that it's a long double.
-// cout << sizeof(num1) << endl;
-// cout << sizeof(num2) << endl;
-// cout << sizeof(num3) << endl;
-//*************Reference Variables*************
-float div = 234.89;
-float & mil = div;
-//cout <<"div = " << div << endl;
-//cout <<"mil = " << mil << endl;
-//*************Typecasting*************
-cout << "The value of div is "<<div<<endl;
-cout << "The integer value of div is " << (int)div << endl;
+    // int result = fact(5);  // function 1
+    // cout << result;
+    // int num;
+    // cout << "Enter the number: ";
+    // cin >> num;
+    // int result = ForLoop(num);    // function 2
+    // cout << result<< endl;
+    float num1 = 45.73; // by default c++ treats this as a double
+    float num2 = 45.73f; // we use f for tell the compiler that it's a floating point value
+    long double num3 = 45.73l; // we use l for tell the compiler that it's a long double.
+    // cout << sizeof(num1) << endl;
+    // cout << sizeof(num2) << endl;
+    // cout << sizeof(num3) << endl;
+    //*************Reference Variables*************
+    float div = 234.89;
+    float & mil = div;
+    //cout <<"div = " << div << endl;
+    //cout <<"mil = " << mil << endl;
+    //*************Typecasting*************
+    // cout << "The value of div is "<<div<<endl;
+    // cout << "The integer value of div is " << (int)div << endl;
+    int maximunNumber = findMax(23,44,21);
+    // cout << maximunNumber << endl;
+    //*************Hard coded partten printing*************
+    // cout <<"*\n* *\n* * *\n* * * *\n* * * * *"<< endl;
+    // dynamicPartten(5);
+    // dynamicRightangleTriangle(5);
+    int array[] = {12,23,34,45,66,78,79,89,90,100};
+    for(int x:array){
+        cout << x<< " ";
+    }
+    // cout << array[2] << endl;
+    int length = sizeof(array)/sizeof(array[0]);
+    cout <<"\n\n" << length << "\n\n";
+//arrayPrint(array,length);
     return 0;
 }
