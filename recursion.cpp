83e671faed56cceccfd892262a7cb25ea0b3cd5f
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-05 05:41:13 +0530

    feat: find out the sumation of a number thro ugh recursion

--- recursion.cpp
+++ recursion.cpp
@@ -8,12 +8,23 @@ int FactorialCalculation(int num){
         return num*FactorialCalculation(num-1);
     }
 }
+int SumasionCalculation(int num){
+    if(num<=0){
+        return 0;
+    }
+    else{
+        return num+SumasionCalculation(num - 1);
+    }
+}
 int main(){
     int num;
     cout<<"Enter the number here : ";
     cin >> num;
     cout <<"\n";
     int t = FactorialCalculation(num);
-    cout <<"The factorial of "<<num<<" is "<<t<<"."<< endl;
+    //cout <<"The factorial of "<<num<<" is "<<t<<"."<< endl;
+    int sum = SumasionCalculation(num);
+    cout << "The Sumation of "<<num<<" is "<<sum<<"."<<endl;
+    
     return 0;
 }
