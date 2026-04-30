4d4225ac3d2cc25997ad337b6ed61ca6f4d72ba1
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-04-30 22:15:06 +0530

    feat: add the challange of my sister

---
+++ Learning-DSA/vectors(cpp)/sister's_challange.cpp
@@ -0,0 +1,15 @@
+/*
+​"How about this? Since we're stuck here watching the rain, let’s do a Weighted Average Grade Calculator.
+​Imagine you have two vectors:
+​One for your Assignment Scores (out of 100).
+​One for the Weightage of each assignment (like 0.2 for 20%, 0.3 for 30%, etc.).
+​The Challenge:
+​Write a program that takes these two vectors, multiplies the score by its weight, and calculates your final 'Cumulative Grade.'
+​Bonus Points: If the final grade is below 80, the program should print: 'System Error: Needs more chocolate for brain power.'
+​If it's above 90, it prints: 'Princess Approved: Commander is a genius.'"
+*/
+#include <iostream>
+int main(){
+
+    return 0;
+}
