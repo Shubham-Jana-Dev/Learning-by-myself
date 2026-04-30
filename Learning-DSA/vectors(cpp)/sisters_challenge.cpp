b92d6e4c607bf389a51ae95b0143a9073f6dee85
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-04-30 23:16:08 +0530

    feat: sister's CGPA calculator challenge has done

---
+++ Learning-DSA/vectors(cpp)/sisters_challenge.cpp
@@ -0,0 +1,47 @@
+/*
+​"How about this? Since we're stuck here watching the rain, let’s do a Weighted Average Grade Calculator.
+​Imagine you have two vectors:
+​One for your Assignment Scores (out of 100).
+​One for the Weightage of each assignment (like 0.2 for 20%, 0.3 for 30%, etc.).
+​The Challenge:
+​Write a program that takes these two vectors, multiplies the score by its weight, and calculates your final 'Cumulative Grade.'
+​Bonus Points: If the final grade is below 80, the program should print: 'System Error: Needs more chocolate for brain power.'
+​If it's above 90, it prints: 'Princess Approved: Commander is a genius.'"
+*/
+#include <iostream>
+#include <vector>
+int main(){
+std::vector<float> Assignment_scores;
+std::vector<float> Assignment_Weightage;
+std::vector<std::string> Assignment_sub;
+Assignment_sub.push_back("Mathematics");
+Assignment_scores.push_back(80);
+Assignment_Weightage.push_back(0.4);
+Assignment_sub.push_back("AI & ML");
+Assignment_scores.push_back(70);
+Assignment_Weightage.push_back(0.6);
+Assignment_sub.push_back("COA");
+Assignment_scores.push_back(89);
+Assignment_Weightage.push_back(0.3);
+Assignment_sub.push_back("DSA");
+Assignment_scores.push_back(80);
+Assignment_Weightage.push_back(0.9);
+int num = Assignment_scores.size();
+float cc = 0;
+for (int i = 0; i<num; i++){
+    cc = cc + (Assignment_scores[i] * Assignment_Weightage[i]);
+    std::cout<<"Final score of: " << Assignment_sub[i] << " is "<< cc << std::endl;
+}
+if(cc>90){
+    std::cout<<"Princess Approved: Commander is a genius."<<std::endl;
+}
+else if(cc>80){
+    std::cout<<"Good"<<std::endl;
+}
+else{
+    std::cout<<"System Error: Needs more chocolate for brain power."<<std::endl;
+}
+
+
+    return 0;
+}
