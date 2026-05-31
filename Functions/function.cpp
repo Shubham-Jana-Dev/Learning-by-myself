82b43cfbf42cef41164cde91bb107ed0f708c500
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-05-31 23:30:35 +0530

    feat: add a sipmle example of a function that can display hello

--- Functions/function.cpp
+++ Functions/function.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+void sayhello(){
+ std::cout<<"Hello Shubham"<<std::endl;
+}
 int main(){
-
+sayhello();
+std::cout<<"------Funtion inside a loop-----"<<std::endl;
+for(int i = 0; i <= 10; i++){
+    sayhello();
+}
     return 0;
 }
