84a97f24383ce99f49eb61178f1d522fb0fb6481
Shubham Jana <devshubhamjana@gmail.com>
2026-07-24 04:52:51 +0530

    Time: 0 ms (100%), Space: 44.7 MB (66.95%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,38 +3,16 @@ class Solution {
         int start = 0;
         int end = nums.length - 1;
         int mid;
-        int result = 99;
-        if(target > nums[nums.length - 1]){
-            return (nums.length);
-        }
-        if(nums.length ==1){
-            return 0;
-        }
-        while(start<=end){
-            mid = start + (end-start)/2;
-            if(nums[mid]>target){
-                end = mid-1;
-                if(end > 0){
-                    result = end;
-                }else{
-                    result = mid;
-                }
-            }else if(nums[mid]<target){
-                start = mid+1;
-                result = start ;
-               if(mid == nums.length-2 || target < nums[mid+1]){
-                return result;
-               }
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-            else {
-                result = mid ;
-                return result;
-            }
-
         }
-        // if((target<nums[nums.length - 1] && target>nums[nums.length-2])){
-        //     return nums.length - 2;
-        // }
-        return result;
+        return start;
     }
 }
