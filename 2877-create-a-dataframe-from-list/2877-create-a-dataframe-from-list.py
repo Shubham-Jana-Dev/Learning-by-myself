25a3b5bff1b2c5fa564e60df72e3b3b028f023ca
Shubham Jana <devshubhamjana@gmail.com>
2026-08-14 08:52:31 +0530

    Time: 269 ms (34.52%), Space: 66 MB (16.99%) - LeetHub

--- 2877-create-a-dataframe-from-list/2877-create-a-dataframe-from-list.py
+++ 2877-create-a-dataframe-from-list/2877-create-a-dataframe-from-list.py
@@ -1,16 +1,4 @@
 import pandas as pd
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    the_disctionary = {}
-    student_id = []
-    age = []
-    for i in student_data:
-        student_id.append(i[0])
-    for j in student_data:
-        age.append(j[1])
-    the_disctionary={
-        "student_id" : student_id,
-        "age": age
-    }
-    df = pd.DataFrame(the_disctionary)
-    return df
+    return pd.DataFrame(student_data, columns=["student_id","age"])
