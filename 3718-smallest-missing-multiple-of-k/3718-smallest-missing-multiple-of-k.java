c07bccf6a0b8205f94598d1291c05b63a4cbd11b
Shubham Jana <devshubhamjana@gmail.com>
2026-08-15 23:27:26 +0530

    Time: 5 ms (19.64%), Space: 45.3 MB (57.31%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,11 +1,11 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
         Arrays.sort(nums);
-        for(int i = 1; i < 1000; i++){
+        for(int i = k; ;i += k){
             if(i%k == 0 && !(Arrays.binarySearch(nums, i) >= 0)){
                 return i;
             }
         }
-        return -1;
+        //return -1;
     }
 }
